fcc0da45f8512e22be4733cf7cf128e924c14c6c
ZeroPath <noreply@zeropath.com>
2024-07-14 21:34:18 +0000

    Added sanitization functions and comments to prevent XSS attacks.

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ include 'utils.php';
  * Sanitize the user input from the GET request
  * This helps prevent XSS attacks by converting special characters to HTML entities.
  */
+/**
+ * Sanitize the user input from the GET request
+ * This helps prevent XSS attacks by converting special characters to HTML entities.
+ */
+include 'utils.php';
+
 echo sanitize_input($_GET["search"]);
 
 ?>
--- utils.php
+++ utils.php
@@ -10,4 +10,22 @@ function sanitize_input($input) {
     return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 }
 
-?>
+/**
+ * Sanitize user input to prevent XSS attacks.
+ *
+ * @param string $input The user input to sanitize.
+ * @return string The sanitized input.
+ */
+function sanitize_input($input) {
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
+
+/**
+ * Sanitize user input to prevent XSS attacks.
+ *
+ * @param string $input The user input to sanitize.
+ * @return string The sanitized input.
+ */
+function sanitize_input($input) {
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
