8301e4724337e80a258c3de972dc0e0c2568fe07
ZeroPath <noreply@zeropath.com>
2024-07-14 21:32:58 +0000

    Added comments to explain the purpose of the sanitize_input function.

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
+<?php
+
 // Include utility functions
 include 'utils.php';
 
-// Sanitize the user input from the GET request
+/**
+ * Sanitize the user input from the GET request
+ * This helps prevent XSS attacks by converting special characters to HTML entities.
+ */
 echo sanitize_input($_GET["search"]);
+
+?>
 
 ?>
