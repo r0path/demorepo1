ffd12c18010ea2b2e38099400c17c104a139ccc8
ZeroPath <noreply@zeropath.com>
2024-07-15 04:20:44 +0000

    Added a new sanitization function in utils5934.php and updated posts.php to use it for input sanitization.

---
+++ utils5934.php
@@ -0,0 +1,8 @@
+<?php
+
+function sanitize_input5934($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+    return $data;
+}
