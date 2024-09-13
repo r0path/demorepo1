a4026c2cad8314f485013ba8fe2511fbd1f67aac
r0path <raphael@zeropath.com>
2024-09-12 17:06:12 -0700

    Update posts.php

--- posts.php
+++ posts.php
@@ -5,6 +5,7 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
+//
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST['title'];
     $content = $_POST['content'];
