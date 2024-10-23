1a2210ff86f10ac58c2865c2fb8924dda7c73414
Raphael <raphael@zeropath.com>
2024-10-22 20:33:07 -0700

    Update everythingisfine.py

--- everythingisfine.py
+++ everythingisfine.py
@@ -1,4 +1,19 @@
-import os
+from django.shortcuts import render
+from django.http import HttpResponse
+import subprocess
+import logging
 
-print("hi")
-# test
+def process_command(request):
+    # Get user input safely
+    user_input = request.GET.get('command', '')
+    
+    # Log the request 
+    logging.info(f"Received command request: {user_input}")
+    
+
+    # Execute command safely
+    try:
+        output = subprocess.check_output([user_input], shell=False)
+        return HttpResponse(output)
+    except subprocess.CalledProcessError:
+        return HttpResponse("Error executing command")
