b4f435de7ec80e6153f69d615dd06abf2ba6872b
Raphael <raphael@zeropath.com>
2025-01-17 16:00:21 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,5 @@
 <?php
 
-
-
-//asdasd//asdasd//asdasd//asdasd//asdasd
-
-//asdasd
-//asdasd//asdasd//asdasd//asdasd//asdasd
-//asdasd
-
-
-//asdasd
-
-//asdasd
-//asdasd
-
-//asdasd
-//asdasd
-//asdasd
-//asdasd
-//asdasd
-//asdasd
-
-//asdasd
-
-// test
-// test
-// test// test// test
-// test// test// test
-// test// test// test
-// test// test// test
-
-//
-
-//
-
-//
-// test// test// test
-// test// test
-
-// 123
-
-// 56234
-
-// test
-// test// test// test
-
-
 echo "<h>" +$_GET["search"] + "</h>";
 
 ?>
