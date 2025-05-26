457a137c817462540484d1cebb0d2b9706e0ae8f
ZeroPath <noreply@zeropath.com>
2025-05-26 10:45:28 +0000

    docs: Add comments explaining XSS vulnerability fix in index.php

--- test/index.php
+++ test/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Fix XSS vulnerability by:
+// 1. Adding isset() check to prevent undefined index errors
+// 2. Using htmlspecialchars() to escape special characters
 $search = isset($_GET["searc"]) ? $_GET["searc"] : '';
 echo htmlspecialchars($search, ENT_QUOTES, 'UTF-8');
 
