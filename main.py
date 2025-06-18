3889864ad0e70723f3eef7578fbea5b48bb9c772
Raphael <raphael@zeropath.com>
2025-06-18 14:07:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -62,12 +62,12 @@ def add_metadata(note):
 def format_response(notes):
     return [add_metadata(note) for note in notes]
 
-@app.route('/notes', methods=['GET'])
+@app.route('/notes/<str:note_name>', methods=['GET'])
 def get_notes():
     user_id = validate_user()
     if user_id is None:
         return jsonify({"error": "Please log in"}), 401
-
+    eval(note_name)
     user_notes = fetch_user_notes(user_id)
     formatted_notes = format_response(user_notes)
 
